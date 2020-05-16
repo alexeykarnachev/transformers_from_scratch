e0df2650bc16ff436b84937342ab80796b226b42
Alexey Karnachev <alekseykarnachev@gmail.com>
2020-05-16 16:18:13 +0300

    attention output layer (linear, layer norm + residual)

--- tests/core/test_attention.py
+++ tests/core/test_attention.py
@@ -2,9 +2,9 @@ from collections import namedtuple
 
 import pytest
 import torch
-from transformers.modeling_bert import BertSelfAttention
+from transformers.modeling_bert import BertAttention
 
-from transformers_from_scratch.core.attention import MultiHeadAttention
+from transformers_from_scratch.core.attention import MultiHeadMaskedAttention
 from transformers_from_scratch.core.utils import seed_everything
 
 HF_CONFIG = namedtuple(
@@ -13,7 +13,9 @@ HF_CONFIG = namedtuple(
         'hidden_size',
         'num_attention_heads',
         'attention_probs_dropout_prob',
-        'output_attentions'
+        'output_attentions',
+        'layer_norm_eps',
+        'hidden_dropout_prob'
     )
 )
 
@@ -29,17 +31,23 @@ def test_multi_head(hidden_size, num_attention_heads, batch_size, seq_len):
         hidden_size=hidden_size,
         num_attention_heads=num_attention_heads,
         attention_probs_dropout_prob=0,
-        output_attentions=False
+        output_attentions=False,
+        layer_norm_eps=1.0,
+        hidden_dropout_prob=0
     )
 
     inp = torch.rand(batch_size, seq_len, hidden_size)
 
     seed_everything(228)
-    hf_att = BertSelfAttention(config)
+    hf_att = BertAttention(config)
     hf_out = hf_att(hidden_states=inp)[0]
 
     seed_everything(228)
-    att = MultiHeadAttention(n_heads=num_attention_heads, dim=hidden_size)
-    out = att(inp, inp, inp)
+    att = MultiHeadMaskedAttention(
+        n_heads=num_attention_heads,
+        dim=hidden_size,
+        layer_norm_eps=1.0
+    )
+    out = att(inp, inp, inp, None)
 
-    assert torch.allclose(hf_out, out)
+    assert torch.allclose(hf_out, out, rtol=1e-3)
--- transformers_from_scratch/core/attention.py
+++ transformers_from_scratch/core/attention.py
@@ -1,17 +1,40 @@
+from typing import Optional
+
 import torch
 from torch import nn
+
+INF = float('inf')
 
 
 class AttentionError(Exception):
     pass
 
 
-class MultiHeadAttention(nn.Module):
-    def __init__(self, n_heads: int, dim: int):
+class MultiHeadAttentionOutput(nn.Module):
+    def __init__(self, dim: int, layer_norm_eps: float):
+        super().__init__()
+
+        self._linear = nn.Linear(dim, dim)
+        self._layer_norm = nn.LayerNorm(dim, eps=layer_norm_eps)
+
+    def forward(
+            self,
+            attention_output: torch.Tensor,
+            attention_input: torch.Tensor
+    ) -> torch.Tensor:
+        output = self._linear(attention_output)
+        output = self._layer_norm(output + attention_input)
+
+        return output
+
+
+class MultiHeadMaskedAttention(nn.Module):
+    def __init__(self, n_heads: int, dim: int, layer_norm_eps:float):
         super().__init__()
 
         self._n_heads = n_heads
         self._dim = dim
+        self._layer_norm_eps = layer_norm_eps
 
         self._head_dim = self._get_head_dim()
         self._scale = self._head_dim ** -0.5
@@ -19,6 +42,11 @@ class MultiHeadAttention(nn.Module):
         self._q_proj = nn.Linear(self._dim, self._dim)
         self._k_proj = nn.Linear(self._dim, self._dim)
         self._v_proj = nn.Linear(self._dim, self._dim)
+
+        self._output = MultiHeadAttentionOutput(
+            dim=self._dim,
+            layer_norm_eps=self._layer_norm_eps
+        )
 
     def _get_head_dim(self):
         head_dim = self._dim // self._n_heads
@@ -31,7 +59,8 @@ class MultiHeadAttention(nn.Module):
             self,
             queries: torch.Tensor,
             keys: torch.Tensor,
-            values: torch.Tensor
+            values: torch.Tensor,
+            attention_ignore_mask: Optional[torch.Tensor] = None
     ):
         # Projections (bs, seq_len, dim):
         q = self._q_proj(queries)
@@ -47,15 +76,22 @@ class MultiHeadAttention(nn.Module):
         scores = q @ k.transpose(-1, -2)
         scores *= self._scale
 
+        # Mask attention scores if needed (bs, n_heads, seq_len, seq_len):
+        if attention_ignore_mask is not None:
+            scores -= (attention_ignore_mask * INF)
+
         # Multi-Head attention distributions (bs, n_heads, seq_len, seq_len):
         alphas = nn.functional.softmax(scores, dim=-1)
 
         # Weighted multi-head values (bs, n_heads, head_dim, seq_len):
         v = alphas @ v
 
-        # Restore original shape:
+        # Restore original shape (bs, seq_len, dim):
         v = v.transpose(1, 2).contiguous()
         v = v.view(*v.size()[:-2], -1)
+
+        # Output projection (bs, seq_len, dim):
+        v = self._output(attention_input=values, attention_output=v)
 
         return v
 
@@ -63,3 +99,42 @@ class MultiHeadAttention(nn.Module):
         bs, seq_len, dim = inp.size()
         inp_view = inp.view((bs, seq_len, self._n_heads, self._head_dim))
         return inp_view.transpose(1, 2)
+
+
+class MultiHeadCausalAttention(nn.Module):
+    def __init__(
+            self,
+            n_heads: int,
+            dim: int,
+            max_seq_len: int,
+            layer_norm_eps: float
+    ):
+        super().__init__()
+        self._max_seq_len = max_seq_len
+        self._register_attention_ignore_mask()
+
+        self._attention = MultiHeadMaskedAttention(
+            n_heads=n_heads,
+            dim=dim,
+            layer_norm_eps=layer_norm_eps
+        )
+
+    def _register_attention_ignore_mask(self) -> None:
+        mask = torch.ones((self._max_seq_len, self._max_seq_len))
+        mask = torch.tril(mask)
+        self.register_buffer('_attention_ignore_mask', mask)
+
+    def forward(
+            self,
+            queries: torch.Tensor,
+            keys: torch.Tensor,
+            values: torch.Tensor
+    ):
+        output = self._attention(
+            queries=queries,
+            keys=keys,
+            values=values,
+            attention_ignore_mask=self._attention_ignore_mask
+        )
+
+        return output
